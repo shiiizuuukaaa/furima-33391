d8b9513b98a79ebe62c38fe4cde2a2f8180b4cb9
shiiizuuukaaa <szk0913@icloud.com>
2021-04-11 21:14:25 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :email, uniqueness: { case_sensitive: true }
+  validates :password, length: { minimum: 6 },
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i } 
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  validates :birth_date, presence: true
+
 end
