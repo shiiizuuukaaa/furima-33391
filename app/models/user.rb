a6e872723a2d003f6a2f57f005c74d4db849d03a
shiiizuuukaaa <szk0913@icloud.com>
2021-04-16 11:47:55 +0900

    テストコードの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,14 @@ class User < ApplicationRecord
   
   validates :nickname, presence: true
   validates :email, uniqueness: { case_sensitive: true }
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i } 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "Include both letters and numbers"}
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Full-width characters"} do
     validates :first_name
     validates :last_name
   end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: "Full-width katakana characters" } do
     validates :first_name_kana
     validates :last_name_kana
   end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    email                 {Faker::Internet.free_email}
+    password              {'aaa111'}
+    password_confirmation {password}
+    first_name            {'苗字'}
+    last_name             {'名前'}
+    first_name_kana       {'ミョウジ'}
+    last_name_kana        {'ナマエ'}
+    birth_date            {20000101}
+  end
+end
