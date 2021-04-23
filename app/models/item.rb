d27e288b1b83885f35507382fa16cff28cdaef2c
shiiizuuukaaa <szk0913@icloud.com>
2021-04-23 15:38:37 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,4 +16,20 @@ class Item < ApplicationRecord
   end
 
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
+  with options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
+  
 end
