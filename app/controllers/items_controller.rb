09adb51094df126707298ab86954aa205e9eae98
shiiizuuukaaa <szk0913@icloud.com>
2021-04-24 14:16:45 +0900

    商品出品ページの表示、商品の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,25 @@
 class ItemsController < ApplicationController
-  
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
-  def message_params
+  def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
-  belpngs_to :user
+  belongs_to :user
   has_one :oder
   has_one_attached :image
 
-  with options presence: true do
+  with_options presence: true do
     validates :image
     validates :name
     validates :info
@@ -24,12 +24,12 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :scheduled_delivery
 
-  with options numericality: { other_than: 1 } do
+  with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :sales_status_id
     validates :shipping_fee_status_id
     validates :prefecture_id
     validates :scheduled_delivery_id
   end
-  
+
 end
