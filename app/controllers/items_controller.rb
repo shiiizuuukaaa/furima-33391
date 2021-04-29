c0e590c3287ac987618f163da0ccd2a62eba86a5
shiiizuuukaaa <szk0913@icloud.com>
2021-04-29 19:59:20 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,9 +51,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless current_user == @item.user
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless current_user == @item.user
   end
-
 end
