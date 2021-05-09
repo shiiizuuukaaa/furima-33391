235c5b3fa7442373380f946d49e10fd0d10ec1df
shiiizuuukaaa <szk0913@icloud.com>
2021-05-09 13:50:14 +0900

    誤字修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,4 @@
-class OderAdress
+class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
 
@@ -15,7 +15,7 @@ class OderAdress
   validates :phone_number, numericality: {message: 'Input only number'}
 
   def save
-    Oder.create(user_id: user.id, item_id: item.id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, oder_id: oder.id)
+    order = Order.create(user_id: user.id, item_id: item.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
