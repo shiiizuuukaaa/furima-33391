a288269ffadaefe10be70f2d3df80c7ef477495b
shiiizuuukaaa <szk0913@icloud.com>
2021-05-16 23:08:47 +0900

    move_to_root_pathメソッドを追記

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_root_path, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -25,6 +26,10 @@ class OrdersController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def move_to_root_path
+    redirect_to root_path if current_user.id == (@item.user_id || @item.order.present?)
   end
 
 end
