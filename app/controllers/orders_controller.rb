cc505871e49b5f8b1d473bffac7e528b5bbe594e
shiiizuuukaaa <szk0913@icloud.com>
2021-05-16 17:45:05 +0900

    Formオブジェクトを用いた実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,29 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_aaddress_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_aaddress_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
+          .merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
 end
